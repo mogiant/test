c93fc03772ae832e741956427050b46ba1d2e32f
mogiant <1934086216@qq.com>
2025-08-20 15:47:57 +0800

    test1

---
+++ Hello/Hello.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+// int main()
+// {
+//     std::cout << "Hello World!" << std::endl;
+//     return 0;
+// }
+
+// int main()
+// {
+//     int x = 8;
+//     int y = 6;
+//     std::cout << std::endl;
+//     std::cout << x - y << " " << x * y << " " << x + y;
+//     std::cout << std::endl;
+// }
+
+// int main()
+// {
+//     std::cout << "Hello World!" << std::endl;
+//     return 0;
+// }
+
+// int main()
+// {
+//     // Declare a variable to store an integer
+//     int inputNumber;
+
+//     cout << "Enter a integer: ";
+
+//     // store integer given user input
+//     cin >> inputNumber;
+
+//     // The same with text i.e. string data
+//     cout << "Enter your name: ";
+//     string inputName;
+//     cin >> inputName;
+
+//     cout << inputName << " entered " << inputNumber << endl;
+
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     int age = 30;
+//     int* pointToint = &age;
+
+//     // Display the value of pointer variable
+//     cout << "Integer age is at 0x" << pointToint << endl;
+
+//     return 0;
+// }
+
+class MyString
+{
+private:
+    char* buffer;
+
+public:
+    MyString(const char* initString)
+    {
+        if(initString != NULL)
+        {
+            buffer = new char[strlen(initString) + 1];
+            strcpy(buffer, initString);
+        }
+        else
+        {
+            buffer = NULL;
+        }
+    }
+
+    ~MyString()
+    {
+        cout << "Invoking destructor, clearing up" << endl;
+        if(buffer!= NULL)
+        {
+            delete[] buffer;
+        }
+    }
+
+    int GetLength()
+    {
+        return strlen(buffer);
+    }
+
+    char* GetString()
+    {
+        return buffer;
+    }
+};
+
+int main()
+{
+    MyString sayHello("Hello from String Class");
+    cout << "Length of string is " << sayHello.GetLength() << endl;
+    cout << "String is " << sayHello.GetString() << endl;
+
+    return 0;
+}
